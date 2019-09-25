4220db07a32c571c98a6092b4842b0029c9a2bc7
Martin Drahony <martin@mdrahony.online>
2019-09-25 12:07:07 +0100

    home added headings and info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,22 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3 p-5">
+        <div class="col-3 p-4">
             <div>
-                <img src="/svg/MDWDLogo2.3.svg"
-                    style="height:100px;">
+                <img src="/svg/MDWDLogo2.3.svg" style="height:100px;">
             </div>
         </div>
-        <div class="col-9">
+        <div class="col-9 p-5">
+            <div >
+                <h1>Martin Drahony Web Dev</h1>
+                <div class="d-flex">
+                    <div class="pr-5"><strong>987k</strong> Likes</div>
+                    <div class="pr-5"><strong>1.8M</strong> followers</div>
+                    <div class="pr-5"><strong>187</strong> Following</div>
+                </div>
 
+
+            </div>
         </div>
     </div>
 </div>
