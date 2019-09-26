7b20bf613661d543c305eb5c725c487a043ca62f
Martin Drahony <martin@mdrahony.online>
2019-09-26 07:08:03 +0100

    created redirectTo function for user ID

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoginController extends Controller
 {
@@ -25,7 +27,19 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    //protected $redirectTo = '/home';
+
+    public function redirectTo(){
+        
+        // User role
+        $id = Auth::user()->id; 
+        
+        // Check user role
+        
+        return 'profile/' . $id;
+       
+    }
+
 
     /**
      * Create a new controller instance.
