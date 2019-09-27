9beac430b576cbd198be667688eb60cc63f408d5
Martin Drahony <martin@mdrahony.online>
2019-09-27 11:01:32 +0100

    created edit and update routes for profile

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
+Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
+Route::patch('/profile/{user}', 'ProfilesController@update')->name('profile.update');
 Route::get('/p/create', 'PostsController@create');
 Route::get('/p/{post}', 'PostsController@show');
 Route::post('/p', 'PostsController@store');
