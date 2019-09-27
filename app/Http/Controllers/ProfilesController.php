50fda39e76381d038dc2cc03b90790bbb6076ffb
Martin Drahony <martin@mdrahony.online>
2019-09-27 11:01:10 +0100

    index, edit and update in profile controller

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -9,12 +9,36 @@ class ProfilesController extends Controller
 {
     //
 
-    public function index($user)
+    // public function index($user)
+    // {
+    //     //dd($user);
+    //     $user = User::findOrFail($user);
+    //     return view('profiles.index',[
+    //         'user' => $user,
+    //     ]);
+    // }
+
+    public function index(User $user)
     {
-        //dd($user);
-        $user = User::findOrFail($user);
-        return view('profiles.index',[
-            'user' => $user,
+        return view('profiles.index',compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user->profile);
+        return view('profiles.edit', compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+            'image' => ''
         ]);
+
+        auth()->$user->profile->update($data);
+        return redirect("/profile/{$user->id}");
     }
 }
