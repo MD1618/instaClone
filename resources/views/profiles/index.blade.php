7aa011a17a70c5732f2acc0693ff0cea785ca5c1
Martin Drahony <martin@mdrahony.online>
2019-09-28 14:17:41 +0100

    added profile from data and @can functionality

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -3,18 +3,24 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3 pt-5 pl-5">
+        <div class="col-3 pt-5 pl-5 pb-3">
             <div>
-                <img src="/svg/MDWDLogo2.3.svg" style="height:100px;">
+            <img src="/storage/{{ $user->profile->profileImage() }}" class="w-100">
             </div>
         </div>
         <div class="col-9 p-5">
             <div>
                 <div class="d-flex justify-content-between align-items-baseline">
                     <h1>{{ $user->username }}</h1>
+                    @can('update', $user->profile)
                     <a href="/p/create">Add New Post</a>
+                    @endcan
                 </div>
+
+                @can('update', $user->profile)
                 <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+                @endcan
+
                 <div class="d-flex">
                     <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> Posts</div>
                     <div class="pr-5"><strong>1.8M</strong> followers</div>
