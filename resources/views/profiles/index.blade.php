346c845524a69f9a5a5fff0544d1228949ae41c7
Martin Drahony <martin@mdrahony.online>
2019-09-28 14:38:23 +0100

    added follow button and container div

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -5,13 +5,16 @@
     <div class="row">
         <div class="col-3 pt-5 pl-5 pb-3">
             <div>
-            <img src="/storage/{{ $user->profile->profileImage() }}" class="w-100">
+                <img src="/storage/{{ $user->profile->profileImage() }}" class="w-100">
             </div>
         </div>
         <div class="col-9 p-5">
             <div>
                 <div class="d-flex justify-content-between align-items-baseline">
-                    <h1>{{ $user->username }}</h1>
+                    <div class="d-flex align-items-center pb-3">
+                        <h2>{{ $user->username }}</h2>
+                        <button class="btn btn-primary ml-4">Follow</button>
+                    </div>
                     @can('update', $user->profile)
                     <a href="/p/create">Add New Post</a>
                     @endcan
