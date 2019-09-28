cf911db54603e67ae9bdf1c74cd1919c53a137ef
Martin Drahony <martin@mdrahony.online>
2019-09-28 14:16:54 +0100

    created show.blade

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,11 +10,27 @@
         </div>
         <div class="col-4">
             <div>
-                <h3>{{ $post->user->username }}</h3>
-                <p>{{ $post->caption }}</p>
+                <div class="d-flex align-items-center">
+                    <div class="pr-4">
+                        <img src="/storage/{{ $post->user->profile->profileImage() }}" alt="" class="w-100 rounded-circle"
+                            style="max-width:70px;">
+                    </div>
+                    <div>
+                        <div class="font-weight-bold">
+                            <a href="/profile/{{ $post->user->id }}">
+                                <span class="text-dark">
+                                    {{ $post->user->username }} |
+                                </span>
+                            </a>
+                            <a href="" class="pl-3">Follow</a>
+
+                        </div>
+                    </div>
+                </div>
+                <p class="pt-4">{{ $post->caption }}</p>
             </div>
         </div>
     </div>
-    
+
 </div>
 @endsection
