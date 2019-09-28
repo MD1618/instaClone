feebb2ceed65b570dae4d53c7ac723beb69cd7ed
Martin Drahony <martin@mdrahony.online>
2019-09-28 14:16:38 +0100

    added spacing to routes file

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,9 @@ Auth::routes();
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
 Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
 Route::patch('/profile/{user}', 'ProfilesController@update')->name('profile.update');
+
+// order here is important otherwise the {post} will take create instead
 Route::get('/p/create', 'PostsController@create');
 Route::get('/p/{post}', 'PostsController@show');
+
 Route::post('/p', 'PostsController@store');
